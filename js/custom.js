7ec2f270d6e4e633adc5d125d8e7cedd604928d4
Harshada <hyadav8701@conestogac.on.ca>
2025-05-09 21:51:07 -0400

    portfolio

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+(function ($) {
+  "use strict";
+
+  // COLOR MODE
+  $(".color-mode").click(function () {
+    $(".color-mode-icon").toggleClass("active");
+    $("body").toggleClass("dark-mode");
+  });
+
+  // HEADER
+  $(".navbar").headroom();
+
+  // PROJECT CAROUSEL
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true,
+  });
+
+  // SMOOTHSCROLL
+  $(function () {
+    $(".nav-link, .custom-btn-link").on("click", function (event) {
+      var $anchor = $(this);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $($anchor.attr("href")).offset().top - 49,
+          },
+          1000
+        );
+      event.preventDefault();
+    });
+  });
+
+  // TOOLTIP
+  $(".social-links a").tooltip();
+
+  /**
+   * Animate the skills items on reveal
+   */
+  let skillsAnimation = document.querySelectorAll(".skills-animation");
+  skillsAnimation.forEach((item) => {
+    new Waypoint({
+      element: item,
+      offset: "80%",
+      handler: function (direction) {
+        let progress = item.querySelectorAll(".progress .progress-bar");
+        progress.forEach((el) => {
+          console.log(el);
+          el.style.width = el.getAttribute("aria-valuenow") + "%";
+        });
+      },
+    });
+  });
+})(jQuery);
